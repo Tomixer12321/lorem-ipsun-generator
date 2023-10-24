fda2d982b21a240bb15b0d93662e60723f7811d3
Tomixer <tomix.klokar@gmail.com>
2023-10-24 19:37:04 +0200

    create new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data"
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>{
+  const submitForm=(e)=>{
+    e.preventDefault()
+  }
+  return <section>
+    <h1>Lorem ipsum generator</h1>
+    <form onSubmit={submitForm}>
+      <label htmlFor="paragraph">odstavec:</label>
+      <input type="number" id="paragraph" /><br />
+      <input type="submit" value="vygenerovat"/>
+    </form>
+  </section>
 }
-
-export default App;
+export default App
