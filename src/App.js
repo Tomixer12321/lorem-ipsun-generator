92abd793598588999a7ffdf9dc2b8a820de965e9
Tomixer <tomix.klokar@gmail.com>
2023-10-25 21:14:09 +0200

    uprava kodu

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import data from "./data"
-import { useState } from "react"
+import data from "./data";
+import { useState } from "react";
 
-const App=()=>{
-  const [count,setCount]=useState(0)
-  const [paragraph,setParagraph]=useState([])
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [paragraph, setParagraph] = useState([]);
 
-  const submitForm=(e)=>{
-    e.preventDefault()
-    let amount=parseInt(count)
+  const submitForm = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
 
-    setParagraph(data.slice(0,amount))
-
-    if(amount<0){
-      amount=1
-    }else if(amount>5){
-      amount=5
+    if (amount < 0) {
+      amount = 1;
+    } else if (amount > 5) {
+      amount = 5;
     }
-  }
 
-  return <section>
-    <h1>Lorem ipsum generator</h1>
-    <form onSubmit={submitForm}>
-      <label htmlFor="paragraph">odstavec:</label>
-      <input type="number" id="paragraph" onChange={(e)=>{
-        setCount(e.target.value)}}/><br />
-      <input type="submit" value="vygenerovat"/>
-    </form>
-    <article>
-      {
-        paragraph.map((oneParagraph,index)=>{
-          return <p key={index}>{oneParagraph}</p>
-        })
-      }
-    </article>
-  </section>
-}
-export default App
+    setParagraph(data.slice(0, amount));
+  };
+
+  return (
+    <section>
+      <h1>Lorem ipsum generator</h1>
+      <form onSubmit={submitForm}>
+        <label htmlFor="paragraph">odstavec:</label>
+        <input
+          type="number"
+          id="paragraph"
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <br />
+        <input type="submit" value="vygenerovat" />
+      </form>
+      <article>
+        {paragraph.map((oneParagraph, index) => {
+          return <p key={index}>{oneParagraph}</p>;
+        })}
+      </article>
+    </section>
+  );
+};
+export default App;
