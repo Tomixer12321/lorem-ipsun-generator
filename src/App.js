f480a2a7e627fb747878ec99e79535d7340f49ae
Tomixer <tomix.klokar@gmail.com>
2023-10-25 20:51:49 +0200

    added onChanege

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import data from "./data"
 import { useState } from "react"
 
 const App=()=>{
+  const [count,setCount]=useState(0)
+
   const submitForm=(e)=>{
     e.preventDefault()
+    const amount=parseInt(count)
+    console.log(typeof(amount))
   }
+
   return <section>
     <h1>Lorem ipsum generator</h1>
     <form onSubmit={submitForm}>
       <label htmlFor="paragraph">odstavec:</label>
-      <input type="number" id="paragraph" /><br />
+      <input type="number" id="paragraph" onChange={(e)=>{
+        setCount(e.target.value)}}/><br />
       <input type="submit" value="vygenerovat"/>
     </form>
   </section>
